d99c9e1807dc51926a5a911f1bea3c0f4c6390e7
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-06-08 10:03:54 +0530

    Implemented Aync-await functionality in the index.js

--- src/index.js
+++ src/index.js
@@ -50,25 +50,41 @@ app.get('/task/:id',(req, res)=>{
     })
 })
 
-app.post('/users' , (req , res)=>{
+app.post('/users' , async (req , res)=>{
     const user = new User(req.body)
-    user.save().then(()=>{
+    await user.save()
+    try{
         res.send(user)
-    }).catch((error) => {
+    }catch{
         res.status(400)
         res.send('Error' + error)
-    })
+        
+    }
+    // user.save().then(()=>{
+    //     res.send(user)
+    // }).catch((error) => {
+    //     res.status(400)
+    //     res.send('Error' + error)
+    // })
 })
 
-app.post('/tasks' , (req , res) =>{
+app.post('/tasks' , async (req , res) =>{
     const task = new Task(req.body)
+    await task.save()
     
-    task.save().then(()=>{
+    try{
         res.send(task)
-    }).catch((error)=>{
+    }
+    catch{  
         res.status(400)
         res.send('Error' + error)
-    })
+    }
+    // task.save().then(()=>{
+    //     res.send(task)
+    // }).catch((error)=>{
+    //     res.status(400)
+    //     res.send('Error' + error)
+    // })
 })
 
 
