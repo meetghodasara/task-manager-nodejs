1bd346f2be1e14563d0d631c284f8bf0e42c48eb
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-06-08 19:49:30 +0530

    Adding middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcrypt');
 const userRouter  = require('./routers/user')
-const taskRouter = require('./routers/task')
+const taskRouter = require('./routers/task');
+const res = require('express/lib/response');
 const app = express()
 require('./db/mongoose')
 const port = process.env.PORT || 3000
@@ -13,3 +15,18 @@ app.use(userRouter)
 app.listen(port , ()=>{
     console.log('Click the link http://localhost:'+ port);
 })
+
+const saltRounds = 8;
+
+const myFunction = async () => {
+    const myPlaintextPassword  = 'myPasswordss'
+    const hashPassword = await bcrypt.hashSync(myPlaintextPassword, saltRounds);
+    console.log(myPlaintextPassword);
+    console.log(hashPassword);
+    const isMatch = await bcrypt.compare(myPlaintextPassword  , hashPassword)
+    console.log(isMatch)
+}
+myFunction()
+
+
+
