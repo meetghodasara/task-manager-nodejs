64c7215937ae813cb2db4e5d877d82839b7926cd
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-06-08 15:51:52 +0530

    Chaining

--- playground/promise-chaining-users.js
+++ playground/promise-chaining-users.js
@@ -1,14 +1,13 @@
 require("../src/db/mongoose");
 
-const { ConnectionStates } = require("mongoose");
 const User = require("../src/models/users");
 
 const updateAgeAndCount = async (id , age) => { 
-    ConnectionStates user = await User.findByIdAndUpdate(id , {age} )
+    const user = await User.findByIdAndUpdate(id , {age} )
     const count = await User.countDocuments({age})
     return count
 }
-f
+
 updateAgeAndCount('629c8a8bee85d6d03b0c695c' , 5).then((count) => {
     console.log('Total number of people with is ' + count )
 }).catch((e) =>{
